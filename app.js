033a8fe00a670bd4f1c4dda1b6dd12c41cff6852
Dale Severude <dseverude@msn.com>
2019-02-15 12:23:56 -0800

    Update info for projects

--- app.js
+++ app.js
@@ -7,73 +7,133 @@ const projects = [
 		url: 'https://space-station-capstone.herokuapp.com/',
 		image: 'img/ISS.jpg',
 		alt: 'Space Station',
-		description: 'A Full Stack Application which tracks the International Space Station using Mongo, Express, React and Node'
+		description: 'Unit 12: International Space Station Tracker',
+		title: 'Unit 12: Create your own Full Stack App with React',
+		usage: 'Full Stack app',
+		skills: 'Full Stack App',
+		overview: 'This capstone project uses three APIs to track the International Space Station on a map along with current weather conditions below.  Users site locations are tracked in a database.',
+		github: 'https://github.com/severude/Space-Station'
 	},
 	{
 		url: 'https://github.com/severude/Course-Rating-API',
 		image: 'img/Postman.JPG',
 		alt: 'Course-Rating',
-		description: 'A REST API which lets users create, edit and rate courses on the back-end using a Mongo database and Express'
+		description: 'Unit 11: Course Rating API',
+		title: 'Unit 11: Build a REST API',
+		usage: 'Server-side app',
+		skills: 'REST API',
+		overview: 'Build a course rating API that will provide a way for users to administer a school database containing information about courses. Verify REST API routes with Postman.',
+		github: 'https://github.com/severude/Course-Rating-API'
 	},
 	{
 		url: 'https://github.com/severude/Library-Manager',
 		image: 'img/Library-Manager.JPG',
 		alt: 'Library-Manager',
-		description: 'Uses a Sqlite database, Node and Express to search, update and add items to a library app'
+		description: 'Unit 10: Library Manager',
+		title: 'Unit 10: Build a Library Database Manager',
+		usage: 'Server-side app',
+		skills: 'Sqlite web application',
+		overview: 'Create a web application for listing, adding, updating, and deleting books in a library application.',
+		github: 'https://github.com/severude/Library-Manager'
 	},
 	{
-		url: 'https://github.com/severude/Gallery-App',
+		url: 'https://github.com/severude/Gulp-Build',
 		image: 'img/Flickr.JPG',
 		alt: 'Gallery-App',
-		description: 'A photo gallery app that searches for photos using the React framework and the Flickr API'
+		description: 'Unit 9: Photo Gallery App',
+		title: 'Unit 9: Build a React Photo Gallery App',
+		usage: 'Server-side app',
+		skills: 'React app',
+		overview: 'Build a fast and lightweight photo gallery application to access Flickr.  To use this app you must provide your own your own Flickr API Key Tokens.',
+		github: 'https://github.com/severude/Gulp-Build'
 	},
 	{
 		url: 'https://github.com/severude/Gulp-Build',
 		image: 'img/Gulp.jpg',
 		alt: 'Gulp-Build',
-		description: 'A quick and efficient professional workflow using JavaScript, npm and Node'
+		description: 'Unit 8: Build Workflow',
+		title: 'Unit 8: Create a Build Workflow',
+		usage: 'Command line script',
+		skills: 'Gulp scripts',
+		overview: 'Implement a quick and efficient professional workflow to compile, concatenate, minify, optimize and copy a given website into the dist folder.',
+		github: 'https://github.com/severude/Gulp-Build'
 	},
 	{
 		url: 'https://github.com/severude/Twitter-Interface',
 		image: 'img/Twitter.jpg',
 		alt: 'Twitter-Interface',
-		description: 'Uses Node.js and Express to retrieve information from your Twitter account'
+		description: 'Unit 7: Twitter Interface',
+		title: 'Unit 7: Build a Twitter Interface',
+		usage: 'Server-side app',
+		skills: 'Node Web Server',
+		overview: 'Build a Node.js and Express-powered Web Site that can access your Twitter account. To use this app you must provide your own your own personal Twitter Keys and Access Tokens.',
+		github: 'https://github.com/severude/Twitter-Interface'
 	},
 	{
 		url: 'https://github.com/severude/Content-Scraper',
 		image: 'img/Content-Scraping.JPG',
 		alt: 'Content-Scraper',
-		description: "Node.js command line app that uses npm packages to scrape a website and put it's contents into a CSV file"
+		description: 'Unit 6: Data-mining application',
+		title: 'Unit 6: Build a data-mining application',
+		usage: 'Command line script',
+		skills: 'Command line app',
+		overview: 'Create a command-line application to read a web page, extract information, and store the data in a text file.',
+		github: 'https://github.com/severude/Content-Scraper'
 	},
 	{
 		url: 'https://severude.github.io/Employee-Directory',
 		image: 'img/EmployeeDirectory.JPG',
 		alt: 'Employee-Directory',
-		description: 'An employee directory and dynamic modal which is created from an AJAX call to a third-party API'
+		description: 'Unit 5: Employee Directory',
+		title: 'Unit 5: Use an API to create an Employee Directory',
+		usage: 'Client-side app',
+		skills: 'AJAX API calls',
+		overview: 'Create an employee directory by communicating with a third-party API.  Add a clickable modal to allow viewing of further employee details.',
+		github: 'https://github.com/severude/Employee-Directory'
 	},
 	{
 		url: 'https://severude.github.io/Tic-Tac-Toe',
 		image: 'img/TicTacToe.JPG',
 		alt: 'Tic-Tac-Toe',
-		description: 'Play a Tic-Tac-Toe game against the computer.  Uses object-oriented programming principles for data modeling'
+		description: 'Unit 4: Tic-Tac-Toe App',
+		title: 'Unit 4: Build a Tic-Tac-Toe App',
+		usage: 'Client-side app',
+		skills: 'Object-Oriented JavaScript',
+		overview: 'Use JavaScript Object-Oriented Programming to create a Tic-Tac-Toe game against the computer.',
+		github: 'https://github.com/severude/Tic-Tac-Toe'
 	},
 	{
 		url: 'https://severude.github.io/Interactive-Form',
 		image: 'img/InteractiveForm.JPG',
 		alt: 'Interactive-Form',
-		description: 'Form validation that is engaging, interactive, and easy to use for the user'
+		description: 'Unit 3: Interactive Form',
+		title: 'Unit 3: Build an Interactive Form',
+		usage: 'Client-side app',
+		skills: 'Processing HTML Forms',
+		overview: 'Enhance a given form so that it is engaging, interactive, easy to use and validates correctly.',
+		github: 'https://github.com/severude/Interactive-Form'
 	},
 	{
 		url: 'https://severude.github.io/Pagination-Filter',
 		image: 'img/PaginationFilter.JPG',
 		alt: 'Pagination-Filter',
-		description: 'Student listings are dynamically divided into pages with a search button for filtering'
+		description: 'Unit 2: Pagination and Content Filter',
+		title: 'Unit 2: Pagination and Content Filter',
+		usage: 'Client-side app',
+		skills: 'DOM Programming',
+		overview: 'Enhance the usability of a given web page by writing JavaScript to dynamically divide a long list of items into "pages". Add a search feature to display only the students that match specific search criteria.',
+		github: 'https://github.com/severude/Pagination-Filter'
 	},
 	{
 		url: 'https://severude.github.io/Quote-Generator',
 		image: 'img/QuoteGenerator.JPG',
 		alt: 'Quote-Generator',
-		description: 'A randomly selected quote is generated by user clicks or after 15 seconds'
+		description: 'Unit 1: Random Quote Generator',
+		title: 'Unit 1: Build a Random Quote Generator',
+		usage: 'Client-side app',
+		skills: 'HTML, CSS, JavaScript',
+		overview: 'Create a program which displays a randomly selected quote each time the user clicks a button.',
+		github: 'https://github.com/severude/Quote-Generator'
 	}
 ];
 
@@ -83,7 +143,7 @@ const sites = [
 		url: 'https://lifechurchmonroe.org',
 		image: 'img/LifeChurchMonroe.JPG',
 		alt: 'LifeChurchMonroe.org',
-		description: 'LifeChurchMonroe.org - This is a large WordPress site that has more than 20 pages built out'
+		description: 'LifeChurchMonroe.org - A large WordPress site that has more than 20 pages built out'
 	},
 	{
 		url: 'https://severude.github.io/weather-api',
@@ -95,7 +155,7 @@ const sites = [
 		url: 'https://severude.github.io/state-capitals',
 		image: 'img/50-States.JPG',
 		alt: 'state-capitals',
-		description: 'This is a quiz for testing your knowledge of United States state capitals'
+		description: 'A quiz for testing your knowledge of United States state capitals'
 	}
 ];
 
