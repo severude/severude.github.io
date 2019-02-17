86aa767b99ac3d96b255bc81dbc6e400c351bfc6
Dale Severude <dseverude@msn.com>
2019-02-16 20:23:59 -0800

    Add project modal

--- app.js
+++ app.js
@@ -4,78 +4,71 @@
 // Projects Gallery data
 const projects = [
 	{
-		url: 'https://space-station-capstone.herokuapp.com/',
+		url: 'https://space-station-capstone.herokuapp.com',
 		image: 'img/ISS.jpg',
 		alt: 'Space Station',
 		description: 'Unit 12: International Space Station Tracker',
 		title: 'Unit 12: Create your own Full Stack App with React',
-		usage: 'Full Stack app',
 		skills: 'Full Stack App',
 		overview: 'This capstone project uses three APIs to track the International Space Station on a map along with current weather conditions below.  Users site locations are tracked in a database.',
 		github: 'https://github.com/severude/Space-Station'
 	},
 	{
-		url: 'https://github.com/severude/Course-Rating-API',
+		url: 'http://localhost:5000',
 		image: 'img/Postman.JPG',
 		alt: 'Course-Rating',
 		description: 'Unit 11: Course Rating API',
 		title: 'Unit 11: Build a REST API',
-		usage: 'Server-side app',
 		skills: 'REST API',
 		overview: 'Build a course rating API that will provide a way for users to administer a school database containing information about courses. Verify REST API routes with Postman.',
 		github: 'https://github.com/severude/Course-Rating-API'
 	},
 	{
-		url: 'https://github.com/severude/Library-Manager',
+		url: 'http://localhost:3000',
 		image: 'img/Library-Manager.JPG',
 		alt: 'Library-Manager',
 		description: 'Unit 10: Library Manager',
 		title: 'Unit 10: Build a Library Database Manager',
-		usage: 'Server-side app',
 		skills: 'Sqlite web application',
 		overview: 'Create a web application for listing, adding, updating, and deleting books in a library application.',
 		github: 'https://github.com/severude/Library-Manager'
 	},
 	{
-		url: 'https://github.com/severude/Gulp-Build',
+		url: 'http://localhost:3000',
 		image: 'img/Flickr.JPG',
 		alt: 'Gallery-App',
 		description: 'Unit 9: Photo Gallery App',
 		title: 'Unit 9: Build a React Photo Gallery App',
-		usage: 'Server-side app',
 		skills: 'React app',
 		overview: 'Build a fast and lightweight photo gallery application to access Flickr.  To use this app you must provide your own your own Flickr API Key Tokens.',
-		github: 'https://github.com/severude/Gulp-Build'
+		github: 'https://github.com/severude/Gallery-App'
 	},
 	{
-		url: 'https://github.com/severude/Gulp-Build',
+		url: 'This is a command line script',
 		image: 'img/Gulp.jpg',
 		alt: 'Gulp-Build',
 		description: 'Unit 8: Build Workflow',
 		title: 'Unit 8: Create a Build Workflow',
-		usage: 'Command line script',
 		skills: 'Gulp scripts',
 		overview: 'Implement a quick and efficient professional workflow to compile, concatenate, minify, optimize and copy a given website into the dist folder.',
 		github: 'https://github.com/severude/Gulp-Build'
 	},
 	{
-		url: 'https://github.com/severude/Twitter-Interface',
+		url: 'http://localhost:3000',
 		image: 'img/Twitter.jpg',
 		alt: 'Twitter-Interface',
 		description: 'Unit 7: Twitter Interface',
 		title: 'Unit 7: Build a Twitter Interface',
-		usage: 'Server-side app',
 		skills: 'Node Web Server',
 		overview: 'Build a Node.js and Express-powered Web Site that can access your Twitter account. To use this app you must provide your own your own personal Twitter Keys and Access Tokens.',
 		github: 'https://github.com/severude/Twitter-Interface'
 	},
 	{
-		url: 'https://github.com/severude/Content-Scraper',
+		url: 'This is a command line script',
 		image: 'img/Content-Scraping.JPG',
 		alt: 'Content-Scraper',
 		description: 'Unit 6: Data-mining application',
 		title: 'Unit 6: Build a data-mining application',
-		usage: 'Command line script',
 		skills: 'Command line app',
 		overview: 'Create a command-line application to read a web page, extract information, and store the data in a text file.',
 		github: 'https://github.com/severude/Content-Scraper'
@@ -86,7 +79,6 @@ const projects = [
 		alt: 'Employee-Directory',
 		description: 'Unit 5: Employee Directory',
 		title: 'Unit 5: Use an API to create an Employee Directory',
-		usage: 'Client-side app',
 		skills: 'AJAX API calls',
 		overview: 'Create an employee directory by communicating with a third-party API.  Add a clickable modal to allow viewing of further employee details.',
 		github: 'https://github.com/severude/Employee-Directory'
@@ -97,7 +89,6 @@ const projects = [
 		alt: 'Tic-Tac-Toe',
 		description: 'Unit 4: Tic-Tac-Toe App',
 		title: 'Unit 4: Build a Tic-Tac-Toe App',
-		usage: 'Client-side app',
 		skills: 'Object-Oriented JavaScript',
 		overview: 'Use JavaScript Object-Oriented Programming to create a Tic-Tac-Toe game against the computer.',
 		github: 'https://github.com/severude/Tic-Tac-Toe'
@@ -108,7 +99,6 @@ const projects = [
 		alt: 'Interactive-Form',
 		description: 'Unit 3: Interactive Form',
 		title: 'Unit 3: Build an Interactive Form',
-		usage: 'Client-side app',
 		skills: 'Processing HTML Forms',
 		overview: 'Enhance a given form so that it is engaging, interactive, easy to use and validates correctly.',
 		github: 'https://github.com/severude/Interactive-Form'
@@ -119,7 +109,6 @@ const projects = [
 		alt: 'Pagination-Filter',
 		description: 'Unit 2: Pagination and Content Filter',
 		title: 'Unit 2: Pagination and Content Filter',
-		usage: 'Client-side app',
 		skills: 'DOM Programming',
 		overview: 'Enhance the usability of a given web page by writing JavaScript to dynamically divide a long list of items into "pages". Add a search feature to display only the students that match specific search criteria.',
 		github: 'https://github.com/severude/Pagination-Filter'
@@ -130,7 +119,6 @@ const projects = [
 		alt: 'Quote-Generator',
 		description: 'Unit 1: Random Quote Generator',
 		title: 'Unit 1: Build a Random Quote Generator',
-		usage: 'Client-side app',
 		skills: 'HTML, CSS, JavaScript',
 		overview: 'Create a program which displays a randomly selected quote each time the user clicks a button.',
 		github: 'https://github.com/severude/Quote-Generator'
@@ -222,3 +210,63 @@ let workflow = new Vue({
 		skills: skills
 	}
 });
+
+let elementIndex = 0; // Saves the index of last clicked project
+
+// Show overlay when a project is clicked
+$('#gallery').on('click', 'a', function(event){
+	event.preventDefault();
+	
+	// Save project index and show correct overlay arrows
+	elementIndex = $('#gallery a').index(this);
+	showOverlayArrows();
+
+	// Show project information on the overlay
+	let imageLocation = $(this).attr("href");
+	$('#content img').attr("src", imageLocation);
+	let title = $(this).children("p")[2].textContent;
+	$('.title').text(title);
+	let overview = $(this).children("p")[3].textContent;
+	$('.overview').text(overview);
+	let url = $(this).children("p")[4].textContent;
+	$('.url').attr("href", url).text(url);
+	let github = $(this).children("p")[5].textContent;
+	$('.github').attr("href", github).text(github);
+
+	// Show the overlay
+	$('#overlay').show();
+});
+
+// Hide overlay if clicked
+$('#close').click(function() {
+	$('#overlay').hide();
+});
+
+// Logic to determine when to show or hide the overlay arrows
+function showOverlayArrows() {
+	let projectCount = $("#gallery li").length;
+	if(elementIndex+1 === projectCount) {
+		$('.next').hide();
+	} else {
+		$('.next').show();
+	}
+	if(elementIndex-1 < 0) {
+		$('.previous').hide();
+	} else {
+		$('.previous').show();
+	}
+}
+
+// Click to show the previous card
+function previousCard() {
+	$('#gallery a')[elementIndex-1].click();
+};
+
+// Click to show the next card
+function nextCard() {
+	$('#gallery a')[elementIndex+1].click();
+};
+
+// Click handlers for arrow buttons
+$('.previous').on('click', previousCard);
+$('.next').on('click', nextCard);
